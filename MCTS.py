54770f5eab5ac92dcce7e8eae8833285b2516b21
Eric Hedlin <iamerichedlin@gmail.com>
2018-03-12 12:17:23 -0700

    takes into account whose turn it is now

--- MCTS.py
+++ MCTS.py
@@ -1,13 +1,16 @@
 import math
 
 root_node = None
-friendly_turn = 1
 
+#keeps track of whose turn it is
+friendly_turn = -1
+
+#linked list style
 class Node:
-   
-   def __init__(self, parent, board):
+   def __init__(self, parent, board, turn):
+      self.friendly_turn = turn
       self.parent = parent
-      self.board = board      #2
+      self.board = board
 
       #will always be between -1 and 1. the value passed back to the network will be divided by N
       #will be either -1 or 1 depending on if its a terminal state
@@ -18,17 +21,16 @@ class Node:
       self.C = 1     #exploration constant
       self.children = [None]
    def calculateU(self):      #TODO will also have to take into account whose turn it is
-      self.U = self.W*friendly_turn/self.N+self.C*self.P*math.sqrt(math.log(self.parent.N)/(1+self.N))
+      self.U = self.W*self.friendly_turn/self.N+self.C*self.P*math.sqrt(math.log(self.parent.N)/(1+self.N))
       self.N+=1
       self.parent.W += self.W    
       push_value_to_network("W", self, self.W/self.N)    #TODO update the W NN with this value. 
 
 
-#to start, insert root node
-#will need to know whose turn it is. W will will just be inverted if opponent
+#recursively selects hghest value child at each level
 def select(node):
+   friendly_turn*=-1       #alternates turns going down
    if len(node.children) != 0:
-      friendly_turn*=-1       #alternates turns going down
       #recursively select best child
       max_u = 0
       max_node = None
@@ -36,25 +38,26 @@ def select(node):
          if(node.children[x].U > max_u):
             max_u = node.children[x].U
             max_node = node.children[x]
-      return max_node
+      return select(max_node)
    #if it doesn't have children kill the recursion
    else:
       return node
 
 
+#creates all possible children from current state
 def expand(node):
    #for each board reachable by the current board
    possible_outcomes = return_value_from_network() #array of adjacent board positions
    for x in range(len(possible_outcomes)):
-      child = Node(node, possible_outcomes[x])
+      child = Node(node, possible_outcomes[x], friendly_turn)
       node.children.append(child)
       update(child)
 
 
+#propogates new values up the network
 def update(node):
    if(node != root_node):
       node.calculateU()
-      friendly_turn*=-1	         #alternates turns coming back up
       update(node.parent)
 
 def push_value_to_network(type, node, value):
@@ -68,7 +71,7 @@ def return_value_from_network():     #replace this with values from network
 def find_optimal_path(board_layout):
    root_node = Node(None, board_layout)
    while(searching):
-      friendly_turn = 1
+      friendly_turn = -1
       temp_node = select(root_node)
       expand(temp_node)
    most_explore_count = 0
