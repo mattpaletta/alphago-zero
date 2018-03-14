bd94c74ef6d42b1049c7ef08a12b83303ef9e3b1
Jarred Hawkins <jarredhawkins11@gmail.com>
2018-03-14 09:55:27 -0700

    added play loop

--- main.py
+++ main.py
@@ -3,6 +3,24 @@ import sys
 
 from game import Game
 from nnet import NNet
+from coach import Coach
+
+args = dotdict({
+    'numIters': 1000,
+    'numEps': 100,
+    'tempThreshold': 15,
+    'updateThreshold': 0.6,
+    'maxlenOfQueue': 200000,
+    'numMCTSSims': 25,
+    'arenaCompare': 40,
+    'cpuct': 1,
+
+    'checkpoint': './temp/',
+    'load_model': False,
+    'load_folder_file': ('/dev/models/8x100x50','best.pth.tar'),
+    'numItersForTrainExamplesHistory': 20,
+
+})
 
 
 def setup_logging():
@@ -19,5 +37,5 @@ if __name__ == "__main__":
     setup_logging()
     logging.info("Learning Go!")
     game = Game(n=18)
-    nnet = NNet(action_size=game.getActionSize(), board_size_x=18, board_size_y=18)
-
+    nnet = NNet(action_size=game.getActionSize(), board_size_x=19, board_size_y=19)
+    c = Coach(game, nnet, )
