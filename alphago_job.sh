28f18e51d0fc3c65c93be01b70827caecd883d26
Jarred Hawkins <jarredhawkins11@gmail.com>
2018-04-04 22:18:12 -0700

    fixed start script

--- alphago_job.sh
+++ alphago_job.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 #SBATCH --gres=gpu:1        # request GPU "generic resource"
 #SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
@@ -7,5 +6,5 @@
 #SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
 
 module load cuda cudnn python/3.5.2
-source alpha-go-env/bin/activate
+source ../alpha-go-env/bin/activate
 python ./main.py
